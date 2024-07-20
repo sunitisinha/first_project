e3822bd1048e02aacc20d5d2baf991571b47205e
sunitisinha <sunitisinha1904@gmail.com>
2024-07-20 10:34:09 +0530

    pushed to main branch

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,70 +1,80 @@
 // alert("Please atleast you can define >6 characters the username");
 //Two types of data type-
 //Primitive data type--by using single variables to we can store only one data
-//String,Number,Boolean,Undefined,Null
-//String--group of alphanumeric characters--"",''
-var a = "Hello Good Morning..!";
-document.write(a);
-console.log(a);
-console.log(typeof a);
+//Non primitive datat type==by using single variable we can store multiple data
+//Primitive data type-- String,Number,Boolean,Undefined,Null
+//1.String--group of alphanumeric characters--"",''
 
-//Number--Float --point values
+// var a = "Hello Good Morning..!";
+// document.write(a);
+// console.log(a);
+// console.log(typeof a);
 
-let b = 456789;
-console.log(b);
-console.log(typeof b);
-//decimal--without any point
-//Boolean--two values true--when the condition is success
+// //2.Number--Float --point values
+// //Numbers--
+// //float--point values
+// //decimal--123456
 
-var c = true;
-console.log(c);
-console.log(typeof c);
-//false--when the condition is failure
-//undefined--variable with no data
+// //typeof--to know the data type
 
-var d;
-console.log(d);
-console.log(typeof d);
-//null--variable with no value;
+// let b = 456789;
+// console.log(b);
+// console.log(typeof b);
+// //decimal--without any point
 
-var e = null;
-console.log(e);
-console.log(typeof e);
-//Non Primitive Datat type--by using single variables to we can store multiple data
+// //3.Boolean--two values
+// //true--when the condition is success
 
-//Array,Object
-//[array data]--mutable object--we can update the array data
-let arr = ["javascript", 12345, true, 77, 8, false, null, "Hello"];
-console.log(arr);
-console.log(arr.length);
-console.log(arr[4]);
+// var c = true;
+// console.log(c);
+// console.log(typeof c);
+// //false--when the condition is failure
 
-//Length--which is used to check the array length
-//Start from 1
-//Position--we can get specific data with in the array
-//Start from 0
+// //4.undefined--variable with no data
 
-//Array methods
-//pop--which is used to remove the data in end of array
-//push--which is used to add the data in end of array
-//shift--which is used to add the data in front of the array
-//unshift
-//splice
-//slice
-//indexof
-//sort
-//reverse
-//concat
+// var d;
+// console.log(d);
+// console.log(typeof d);
+// //5.null--variable with no value;
 
-arr = ["javascript", 12345, true, 77, 8, false, null, "Hello"];
-console.log(arr);
-arr.pop();
-console.log(arr);
+// var e = null;
+// console.log(e);
+// console.log(typeof e);
+// //Non Primitive Datat type--by using single variables to we can store multiple data
 
-console.log(arr);
-arr.push(true);
-console.log(arr);
+// //Array,Object
+// //[array data]--mutable object--we can update the array data
+// let arr = ["javascript", 12345, true, 77.8, false, null, "Hello", "Moorthy"];
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr[7]);
 
-arr = ["javascript", 12345, true, 77, 8, false, null, "Hello"];
-arr.shift();
-console.log(arr);
+// //Length--which is used to check the array length
+// //Start from 1
+// //Position--we can get specific data with in the array
+// //Start from 0
+
+// //Array methods
+// //pop--which is used to remove the data in end of array
+// //push--which is used to add the data in end of array
+// //shift--which is used to add the data in front of the array
+// //unshift--we can add the data in beginning of array
+// //splice
+// //slice
+// //indexof
+// //sort
+// //reverse
+// //concat
+
+// arr = ["javascript", 12345, true, 77, 8, false, null, "Hello"];
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+// console.log(arr);
+// arr.push(true);
+// console.log(arr);
+
+// arr = ["javascript", 12345, true, 77, 8, false, null, "Hello"];
+// arr.shift();
+// console.log(arr);
